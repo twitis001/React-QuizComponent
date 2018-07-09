1c8b39754b19d0ac51e40dd7f7c56577a95b4210
jeremy.mesias <jeremy.mesias@pointwest.com.ph>
2018-07-09 10:41:15 +0800

    initial commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
     }
 
     render() {
-        return (<div className = "QuizQuestions">
+        return (<div className = "QuizQuestion">
          {quizData.quiz_questions[0].instruction_text};
         </div>);
       }
