5faab6b3dda9a6686908e57b77f0b6eaa450c033
twitis001 <jem_munar@yahoo.com>
2018-07-09 13:58:50 +0800

    test

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,7 +7,7 @@ class QuizQuestion extends Component {
     render (button_text) {
         return(<main>
             <section>
-            <p><QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} /></p>
+            <p>{this.props.quiz_question.instruction_text}</p>
             </section>
             <section className="buttons">
             <ul>
