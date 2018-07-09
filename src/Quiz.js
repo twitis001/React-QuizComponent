9c363bf2775088cf7f2112f0194d88c2c4a14682
twitis001 <jem_munar@yahoo.com>
2018-07-09 12:56:23 +0800

    fixed #9

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
 
     render() {
         return (<div className = "QuizQuestion">
-         {quizData.quiz_questions[0].instruction_text};
+         {quizData.quiz_questions[0].instruction_text}
         </div>);test
       }
 
