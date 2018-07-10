77c3704c2a3593aaeae92c1b2fe738a54f9bbcd3
twitis001 <jem_munar@yahoo.com>
2018-07-10 08:21:57 +0800

    Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,12 +11,19 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
 
+    showNextQuestion () {
+        this.setState((state) => {
+            return {quiz_position: state.quiz_position + 1}
+        })
+    }
+
     render(quiz_question) {
         const isQuizEnd = (this.state.quiz_position -1 == quizData.quiz_questions.length);
         
         return (<div className = "QuizQuestion">
 
-            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} /> }
+            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} 
+                                        showNextQuestionHandler={this.showNextQuestion.bind(this) } /> }
    
         </div>);
       }
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
 
+    handleClick () {
+        return this.props.clickHandler(this.props.button_text)
+    }
+
     render() {
-        return(<li><button>{this.props.button_text}</button></li>);
+        return(<li><button onClick = {this.handleClick.bind(this)} >
+                    {this.props.button_text}</button>
+               </li>);
     }
 }
 
